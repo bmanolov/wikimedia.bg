b9ba2c2f6e90c0b8904bad559edb9f06063f79b5
borislav <b.manolov@gmail.com>
2015-02-25 21:28:07 +0100

    index and readme

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+	<meta charset="utf-8">
+	<title>Уикимедия на български</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container">
+		<div style="width: 800px; margin: 5em auto; text-align: center; font-size: 150%">
+			<h1>Уикимедия на български</h1>
+			<span class="fa fa-wifi" style="font-size:1500%; color:#069"></span>
+			<p>Очаквайте скоро!</p>
+		</div>
+	</div>
+</body>
+</html>
